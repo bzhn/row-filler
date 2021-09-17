1d4d00f3bc4bb2125060b19d0d07e751f1d39af1
bzhn <89320434+bzhn@users.noreply.github.com>
2021-09-17 14:13:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ function generate() {
     query = document.getElementById("querySQL").value;
     separator = document.getElementById("separator").value;
     ready = document.getElementById("readySQL");
-    crop = document.getElementById("crop").checked;
-    cropAmount = document.getElementById("cropAmount").value;
+    useRegexp = document.getElementById("useRegexp").checked;
+    regexRule = document.getElementById("regexRule").value;
 
     rows = values.split("\n");
     newrows = "";
     
-    if (crop && cropAmount) {
-        console.log(cropAmount);
-        c = parseInt(cropAmount);
+    if (useRegexp && regexRule) {
+        console.log(regexRule);
+        // c = parseInt(regexRule);
+        var re = new RegExp(regexRule, "i");
         for (x in rows) {
-            rows[x] = rows[x].substring(0,c)
+            // rows[x] = rows[x].substring(0,c)
+            if (rows[x].match(re)){
+                rows[x] = rows[x].match(re)[0];
+            } else {
+                rows[x] = "";
+            }
             console.log("cropping...");
         }
     }
 
     for (i in rows) {
+        if (rows[i] != "") { 
         newrows += query.replace(separator, rows[i]) + "\n";
+        }
     }
 
     ready.value = newrows;
@@ -28,18 +36,18 @@ function generate() {
 function showCropOptions() {
 
     
-    cropAmount = document.getElementById("cropAmount");
-    crop = document.getElementById("crop").checked;
-    console.log(crop);
+    regexRule = document.getElementById("regexRule");
+    useRegexp = document.getElementById("useRegexp").checked;
+    console.log(useRegexp);
 
-    if (!crop) {
+    if (!useRegexp) {
         console.log('hide\n');
-        cropAmount.style.visibility = "hidden";
+        regexRule.style.visibility = "hidden";
     }
-    if (crop) {
+    if (useRegexp) {
         console.log('show\n');
-        cropAmount.style.visibility = "visible";
+        regexRule.style.visibility = "visible";
     }
     
-    crop = "";
+    useRegexp = "";
 }
